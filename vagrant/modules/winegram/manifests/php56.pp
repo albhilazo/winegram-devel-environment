966be9f616507ae409f736d022bead410b4e10f4
Albert Hilazo <albhilazo@gmail.com>
2016-04-24 18:35:32 +0200

    Added specific ini files for PHP

--- vagrant/modules/winegram/manifests/php56.pp
+++ vagrant/modules/winegram/manifests/php56.pp
@@ -1,7 +1,14 @@
 class winegram::php56
 {
     class { '::php':
-        version => 'latest'
+        version     => 'latest',
+        config_file => '/etc/php5/apache2/php.ini',
+        source      => "puppet:///modules/winegram/php.ini",
+    }
+
+    ::php::conf { 'php-cli.ini':
+        path   => '/etc/php5/cli/php.ini',
+        source => "puppet:///modules/winegram/php-cli.ini",
     }
 
     php::module { ['curl', 'dev', 'json', 'memcached', 'mysql', 'mysqlnd', 'sqlite', 'oauth', 'redis', 'xdebug']: }
